4ca4888b8c776b28f0203a7bd2b7a9362322394d
thezx <thezxzx2@gmail.com>
2024-09-08 11:38:03 -0700

    Operador de asignación

---
+++ 02 - operadores y expresiones/02 - operador de asignacion/ejemplo3.1.cpp
@@ -0,0 +1,21 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    int codigo, coordX, coordY;
+    float fahrenheit, valor;
+
+    codigo = 3467;
+    valor = 10;
+    valor *= 3;
+    coordX = 525;
+    coordY = 725;
+    coordY -= 10;
+    fahrenheit = 123.456;
+    cin.get();
+
+    return EXIT_SUCCESS;
+}
